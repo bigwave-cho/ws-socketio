096e47cb18acee9dca1d3b842c1f18dee8f40cf3
bigwave-cho <hooyaa93@gmail.com>
2023-05-15 14:35:57 +0900

    WebSocket: message type 분류(stringify, parse)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,25 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
 
 //https://developer.mozilla.org/ko/docs/Web/API/WebSocket
 
 // app의 socket은 서버로의 연결을, server의 socket은 어떤 브라우저가 연결되었나(클라)
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
 socket.addEventListener('open', () => {
   console.log('connected to Server!');
 });
 
 socket.addEventListener('message', (message) => {
-  console.log('Just got this: ', message.data.toString(), 'from server');
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener('close', () => {
@@ -23,8 +31,15 @@ function handleSubmit(event) {
   event.preventDefault();
 
   const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  socket.send(makeMessage('new_message', input.value));
   input.value = '';
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+}
+
 messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
